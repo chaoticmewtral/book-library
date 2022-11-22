89ded838470282091d890214a2cdb2afb75817d9
Remy <remy@chaoticmewtral.com>
2022-11-21 19:58:45 -0500

    Add to directory

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let myLibrary = [];
 
-function Book(title, author, genre, length, read) {
+const submitBook = document.querySelector('#submit');
+
+
+
+function Book(title, author, genre, pages, read) {
     this.title = title;
     this.author = author;
     this.genre = genre;
-    this.length = length;
+    this.pages = pages;
     this.read = read;
 }
 
 function addBook() {
-
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const genre = document.querySelector('#genre').value;
+    const pages = document.querySelector('#length').value;
+    const read = document.querySelector('#read').checked;
 }
